05d1a999783cf4cb3a1c755cdd9486de4338c7c1
Alewitox <ale.limi95@gmail.com>
2019-12-06 19:36:51 +0000

    Añadidos los fragment. Falta añadir los intent

--- app/src/main/java/com/example/drawexample/MainActivity.kt
+++ app/src/main/java/com/example/drawexample/MainActivity.kt
@@ -6,6 +6,8 @@ import android.view.View
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,16 +17,48 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         mDrawerLayout = findViewById<View>(R.id.drawer) as DrawerLayout
         mToggle = ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close)
         mDrawerLayout!!.addDrawerListener(mToggle!!)
+        val nvDrawer = findViewById<View>(R.id.nv) as NavigationView
         mToggle!!.syncState()
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        setupDrawerContent(nvDrawer)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return if (mToggle!!.onOptionsItemSelected(item)) {
             true
         } else super.onOptionsItemSelected(item)
+    }
+
+    fun selectItemDrawer(menuItem: MenuItem) {
+        var myFragment: Fragment? = null
+        val fragmentClass: Class<*>
+        fragmentClass = when (menuItem.itemId) {
+            R.id.menuhome -> Series::class.java
+            R.id.menulogin -> IniciarSesion::class.java
+            R.id.menuregister -> Registrarse::class.java
+            R.id.menulogout -> CerrarSesion::class.java
+            else -> Series::class.java
+        }
+        try {
+            myFragment = fragmentClass.newInstance() as Fragment
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        val fragmentManager = supportFragmentManager
+        fragmentManager.beginTransaction().replace(R.id.flcontent, myFragment!!).commit()
+        menuItem.isChecked = true
+        title = menuItem.title
+        mDrawerLayout!!.closeDrawers()
+    }
+
+    private fun setupDrawerContent(navigationView: NavigationView) {
+        navigationView.setNavigationItemSelectedListener { item ->
+            selectItemDrawer(item)
+            true
+        }
     }
 }
